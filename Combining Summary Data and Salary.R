6e27d96906490352f5f0b1ceee522d95c6764c41
Andy Jesse Zhao <andjzhao@rstudio.reed.edu>
2020-11-15 21:48:06 -0800

    Combined summary data and salary

---
+++ Combining Summary Data and Salary.R
@@ -0,0 +1,208 @@
+library(tidyverse)
+library(rvest)
+library(baseballr)
+
+
+# Scrape Data
+pitches1_2016 <- scrape_statcast_savant(start_date = "2016-4-3", 
+                                        end_date = "2016-4-10", 
+                                        player_type='pitcher')
+
+pitches2_2016 <- scrape_statcast_savant(start_date = "2016-4-11", 
+                                        end_date = "2016-4-18", 
+                                        player_type='pitcher')
+
+pitches3_2016 <- scrape_statcast_savant(start_date = "2016-4-19", 
+                                        end_date = "2016-4-26", 
+                                        player_type='pitcher')
+
+pitches4_2016 <- scrape_statcast_savant(start_date = "2016-4-27", 
+                                        end_date = "2016-5-4", 
+                                        player_type='pitcher')
+
+pitches5_2016 <- scrape_statcast_savant(start_date = "2016-5-5", 
+                                        end_date = "2016-5-12", 
+                                        player_type='pitcher')
+
+pitches6_2016 <- scrape_statcast_savant(start_date = "2016-5-13", 
+                                        end_date = "2016-5-20", 
+                                        player_type='pitcher')
+
+
+pitches7_2016 <- scrape_statcast_savant(start_date = "2016-5-21", 
+                                        end_date = "2016-5-28", 
+                                        player_type='pitcher')
+
+pitches8_2016 <- scrape_statcast_savant(start_date = "2016-5-29", 
+                                        end_date = "2016-6-5", 
+                                        player_type='pitcher')
+
+pitches9_2016 <- scrape_statcast_savant(start_date = "2016-6-6", 
+                                        end_date = "2016-6-13", 
+                                        player_type='pitcher')
+
+pitches10_2016 <- scrape_statcast_savant(start_date = "2016-6-14", 
+                                        end_date = "2016-6-21", 
+                                        player_type='pitcher')
+
+pitches11_2016 <- scrape_statcast_savant(start_date = "2016-6-21", 
+                                        end_date = "2016-6-28", 
+                                        player_type='pitcher')
+
+pitches12_2016 <- scrape_statcast_savant(start_date = "2016-6-29", 
+                                         end_date = "2016-7-6", 
+                                         player_type='pitcher')
+
+pitches13_2016 <- scrape_statcast_savant(start_date = "2016-7-7", 
+                                         end_date = "2016-7-14", 
+                                         player_type='pitcher')
+
+pitches14_2016 <- scrape_statcast_savant(start_date = "2016-7-15", 
+                                         end_date = "2016-7-22", 
+                                         player_type='pitcher')
+
+pitches15_2016 <- scrape_statcast_savant(start_date = "2016-7-23", 
+                                         end_date = "2016-7-30", 
+                                         player_type='pitcher')
+
+pitches16_2016 <- scrape_statcast_savant(start_date = "2016-7-31", 
+                                         end_date = "2016-8-7", 
+                                         player_type='pitcher')
+
+pitches17_2016 <- scrape_statcast_savant(start_date = "2016-8-8", 
+                                         end_date = "2016-8-15", 
+                                         player_type='pitcher')
+
+pitches18_2016 <- scrape_statcast_savant(start_date = "2016-8-22", 
+                                         end_date = "2016-8-29", 
+                                         player_type='pitcher')
+
+pitches17_2016 <- scrape_statcast_savant(start_date = "2016-8-30", 
+                                         end_date = "2016-9-6", 
+                                         player_type='pitcher')
+
+pitches18_2016 <- scrape_statcast_savant(start_date = "2016-9-7", 
+                                         end_date = "2016-9-14", 
+                                         player_type='pitcher')
+
+pitches19_2016 <- scrape_statcast_savant(start_date = "2016-9-15", 
+                                         end_date = "2016-9-22", 
+                                         player_type='pitcher')
+
+pitches20_2016 <- scrape_statcast_savant(start_date = "2016-9-23", 
+                                         end_date = "2016-10-2", 
+                                         player_type='pitcher')
+
+# Create Data Frame
+summary_2016 <- rbind(pitches1_2016,
+                      pitches2_2016,
+                      pitches3_2016,
+                      pitches4_2016,
+                      pitches5_2016,
+                      pitches6_2016,
+                      pitches7_2016,
+                      pitches8_2016,
+                      pitches9_2016,
+                      pitches10_2016,
+                      pitches11_2016,
+                      pitches12_2016,
+                      pitches13_2016,
+                      pitches14_2016,
+                      pitches15_2016,
+                      pitches16_2016,
+                      pitches17_2016,
+                      pitches18_2016,
+                      pitches19_2016,
+                      pitches20_2016)
+
+
+
+# Select Variables
+summary_2016 <- summary_2016 %>%
+  dplyr::select(pitch_type, player_name, release_speed, effective_speed, 
+         release_spin_rate, plate_x, plate_z, zone, pfx_x, pfx_z, events)
+
+
+
+# Create Outcome Variables
+summary_2016 <- summary_2016 %>%
+  mutate(`1B` = ifelse(events == "single", 1, 0),
+         `2B` = ifelse(events == "double", 1, 0),
+         `3B` = ifelse(events == "triple", 1, 0),
+         `HR` = ifelse(events == "home_run", 1, 0),
+         `SF` = ifelse(events == "sac_fly", 1, 0),
+         `BB` = ifelse(events == "walk" | events == "hit_by_pitch", 1, 0),
+         `HBP` = ifelse(events == "hit_by_pitch", 1, 0),
+         `SO` = ifelse(events == "strikeout", 1, 0),
+         `AB` = ifelse(events == "single" | events == "double" | events == "triple" | events == "home_run" | events == "strikeout" | events == "strikeout_double_play" | events == "double_play" | events == "field_error" | events == "field_out" | events == "fielders_choice" | events == "force_out" | events == "grounded_into_double_play", 1, 0),
+         `PA` = ifelse(events == "single" | events == "double" | events == "triple" | events == "home_run" | events == "strikeout" | events == "strikeout_double_play" | events == "double_play" | events == "field_error" | events == "field_out" | events == "fielders_choice" | events == "force_out" | events == "grounded_into_double_play" | events == "walk" | events == "hit_by_pitch" | events == "sac_fly", 1, 0))
+
+
+
+# Create Pitch Category Variable
+
+
+# Create Corner Variable (Proxy for Command)
+summary_2016 <- summary_2016 %>%
+  mutate(`corner` = ifelse(zone == 1 | events == 3 | events == 7 | events == 9, 1, 0))
+
+
+
+# Summary Data
+summary_2016 <- summary_2016 %>%
+  group_by(player_name) %>%
+  summarise(`Pitches` = n(),
+            `Average Pitch Speed` = mean(release_speed, na.rm = TRUE),
+            `Average Effective Pitch Speed` = mean(effective_speed, na.rm = TRUE),
+            `Average Spin Rate` = mean(release_spin_rate, na.rm = TRUE),
+            `Average Horizontal Break` = mean(pfx_x, na.rm = TRUE),
+            `Average Vertical Break` = mean(pfx_z, na.rm = TRUE),
+            `Corner %` = (sum(corner = 1))/n(),
+            `BAA` = (sum(`1B` == 1) + sum(`2B` == 1) + sum(`3B` == 1) + sum(`HR` == 1))/(sum(`AB` == 1)),
+            `SLGA` = (sum(`1B` == 1) + 2*sum(`2B` == 1) + 3*sum(`3B` == 1) + 4*sum(`HR` == 1))/(sum(`AB` == 1)),
+            `BABIPA` = (sum(`1B` == 1) + sum(`2B` == 1) + sum(`3B` == 1))/(sum(`AB` == 1) - sum(`HR` == 1) - sum(`SO` == 1) + sum(`SF` == 1)),
+            `wOBAA` = (0.69*sum(`BB` == 1) + 0.72*sum(`HBP` == 1) + 0.89*sum(`1B` == 1) + 1.27*sum(`2B` == 1) + 1.62*sum(`3B` == 1) + 2.1*sum(`HR` == 1))/(sum(`PA` == 1)))
+
+
+
+# Filter
+summary_2016 <- summary_2016 %>%
+  filter(Pitches > 99)
+
+
+
+# Round Numbers
+summary_2016_rounded <- summary_2016 %>%
+  mutate(`Average Pitch Speed` = format(round(`Average Pitch Speed`, 1), nsmall = 1)) %>%
+  mutate(`Average Effective Pitch Speed` = format(round(`Average Effective Pitch Speed`, 1), nsmall = 1)) %>%
+  mutate(`Average Spin Rate` = format(round(`Average Spin Rate`, 1), nsmall = 1)) %>%
+  mutate(`Average Horizontal Break` = format(round(`Average Horizontal Break`, 3), nsmall = 3)) %>%
+  mutate(`Average Vertical Break` = format(round(`Average Vertical Break`, 3), nsmall = 3)) %>%
+  mutate(`Corner %` = format(round(`Corner %`, 3), nsmall = 3)) %>%
+  mutate(`BAA` = format(round(`BAA`, 3), nsmall = 3)) %>%
+  mutate(`SLGA` = format(round(`SLGA`, 3), nsmall = 3)) %>%
+  mutate(`BABIPA` = format(round(`BABIPA`, 3), nsmall = 3)) %>%
+  mutate(`wOBAA` = format(round(`wOBAA`, 3), nsmall = 3))
+
+view(summary_2016_rounded)
+
+library(readr)
+pitcher_salaries_2016 <- read_csv("data/pitcher_salaries_2016.csv")
+View(pitcher_salaries_2016)
+
+total_2016<-summary_2016 %>% inner_join(pitcher_salaries_2016)
+
+write_csv(total_2016, "total_2016.csv")
+
+
+total_2016_filtered<-total_2016 %>% filter(salary>550000)
+
+write_csv(total_2016_filtered, "total_2016_filtered.csv")
+
+
+ggplot(total_2016_filtered, aes( x=`Corner %`,y=log(salary))) +
+  geom_point(alpha=0.5,color="dodgerblue",fill="dodgerblue")+
+  theme_classic()
+
+cor(total_2016_filtered$`Corner %`,log(total_2016_filtered$salary))
+
